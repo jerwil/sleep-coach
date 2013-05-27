417bddd76b632b4b8ae5b0c4b7b7f75e04d393e2
Jeremy Wilson <jerwil@gmail.com>
2013-05-27 17:43:49 -0500

    Functions set up, basic fading working

--- sleepcoach/sleepcoach.ino
+++ sleepcoach/sleepcoach.ino
@@ -8,6 +8,15 @@ unsigned long currentTime;
 double second_timer[1] = {0}; // This is use dto keep track of the timer used to tick for each second
 int ticked = 0;
 
+int delay_int = 50;
+int brightness = 0;
+int brightincrease = 1;
+
+int button_state = 0;
+int button_pushed = 0; // This is the indicator that the button was pushed and released
+int button_press_initiate[1];     // storage for button press function
+int button_press_completed[1];    // storage for button press function
+
 
 void setup() {                
   // initialize the digital pin as an output.
@@ -17,6 +26,12 @@ void setup() {
 
 void loop() {
   
+if (tick(delay_int,second_timer) == 1){
+  brightness += brightincrease;
+}
+if (brightness >= 255){brightincrease = -1;}
+else if (brightness <= 0){brightincrease = 1;}
+  analogWrite(LEDPin,brightness);
 }
 
 int button_press (int button_indicator, int button_press_initiated[1], int button_press_complete[1]){
